fd7eb54ba428e6976ab39ae950012b129d798d00
o-seun <61795897+o-seun@users.noreply.github.com>
2020-11-10 02:55:25 +0900

    up

--- src/component/Nowongu.jsx
+++ src/component/Nowongu.jsx
@@ -21,41 +21,41 @@ class Nowongu extends Component {
                 <label htmlFor="junggye">중계동</label>
               </li>
               <li>
-                <label htmlFor="woulgye">공릉동</label>
+                <label htmlFor="woulgye">월계동</label>
               </li>
             </ul>
 
             <div className="service-list service-sanggye">
               <ul>
                 <li>
-                  <span>1. 포레나노원</span><span className="color-red">-</span>
+                  <Link to="/"><span>1. 포레나노원</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>2. 상계주공7단지</span><span className="color-red">-</span>
+                  <Link to="/"><span>2. 상계주공7단지</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>3. 노원롯데캐슬시그니쳐</span><span className="color-red">-</span>
+                  <Link to="/"><span>3. 노원롯데캐슬시그니쳐</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>4. 상계주공9단지</span><span className="color-red">-</span>
+                  <Link to="/"><span>4. 상계주공9단지</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>5. 보람</span><span className="color-red">2▲</span>
+                  <Link to="/"><span>5. 보람</span></Link><span className="color-red">2▲</span>
                 </li>
                 <li>
                   <Link to="/sang"><span>6. 상계주공16단지</span></Link><span className="color-red">2▲</span>
                 </li>
                 <li>
-                  <span>7. 상계주공6단지</span><span className="color-red">2▼</span>
+                  <Link to="/"><span>7. 상계주공6단지</span></Link><span className="color-red">2▼</span>
                 </li>
                 <li>
-                  <span>8. 벽산</span><span className="color-red">2▲</span>
+                  <Link to="/"><span>8. 벽산</span></Link><span className="color-red">2▲</span>
                 </li>
                 <li>
-                  <span>9. 상계주공3단지</span><span className="color-red">2▼</span>
+                  <Link to="/"><span>9. 상계주공3단지</span></Link><span className="color-red">2▼</span>
                 </li>
                 <li>
-                  <span>10. 상계주공1단지</span><span className="color-red">신규</span>
+                  <Link to="/"><span>10. 상계주공1단지</span></Link><span className="color-red">신규</span>
                 </li>
               </ul>
             </div>
@@ -63,34 +63,34 @@ class Nowongu extends Component {
             <div className="service-list service-junggye">
               <ul>
                 <li>
-                  <span>1. 중계그린</span><span className="color-red">-</span>
+                  <Link to="/"><span>1. 중계그린</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>2. 중계무지개</span><span className="color-red">-</span>
+                  <Link to="/"><span>2. 중계무지개</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>3. 중계주공5단지</span><span className="color-red">-</span>
+                  <Link to="/"><span>3. 중계주공5단지</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>4. 중계롯데상아</span><span className="color-red">-</span>
+                  <Link to="/"><span>4. 중계롯데상아</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>5. 건영2차</span><span className="color-red">-</span>
+                  <Link to="/"><span>5. 건영2차</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>6. 중계주공2단지</span><span className="color-red">-</span>
+                  <Link to="/"><span>6. 중계주공2단지</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>7. 청구3차</span><span className="color-red">-</span>
+                  <Link to="/"><span>7. 청구3차</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>8. 염광</span><span className="color-red">-</span>
+                  <Link to="/"><span>8. 염광</span></Link><span className="color-red">-</span>
                 </li>
                 <li>
-                  <span>9. 중계주공4단지</span><span className="color-red">1▲</span>
+                  <Link to="/"><span>9. 중계주공4단지</span></Link><span className="color-red">1▲</span>
                 </li>
                 <li>
-                  <span>10. 중계2차한화꿈에그린더퍼스트</span><span className="color-red">신규</span>
+                  <Link to="/"><span>10. 중계2차한화꿈에그린더퍼스트</span></Link><span className="color-red">신규</span>
                 </li>
               </ul>
             </div>
@@ -98,34 +98,34 @@ class Nowongu extends Component {
             <div className="service-list service-woulgye">
               <ul>
                 <li>
-                  <span>1. 첨단대우</span><span className="color-red">2▲</span>
+                  <Link to="/"><span>1. 첨단대우</span></Link><span className="color-red">2▲</span>
                 </li>
                 <li>
-                  <span>2. 첨단동부</span><span className="color-red">1▼</span>
+                  <Link to="/"><span>2. 첨단동부</span></Link><span className="color-red">1▼</span>
                 </li>
                 <li>
-                  <span>3. 첨단남양파크</span><span className="color-red">2▲</span>
+                  <Link to="/"><span>3. 첨단남양파크</span></Link><span className="color-red">2▲</span>
                 </li>
                 <li>
-                  <span>4. 첨단산업기지라인1차</span><span className="color-red">2▼</span>
+                  <Link to="/"><span>4. 첨단산업기지라인1차</span></Link><span className="color-red">2▼</span>
                 </li>
                 <li>
-                  <span>5. 첨단삼성</span><span className="color-red">신규</span>
+                  <Link to="/"><span>5. 첨단삼성</span></Link><span className="color-red">신규</span>
                 </li>
                 <li>
-                  <span>6. 기산</span><span className="color-red">신규</span>
+                  <Link to="/"><span>6. 기산</span></Link><span className="color-red">신규</span>
                 </li>
                 <li>
-                  <span>7. 첨단선경</span><span className="color-red">1▼</span>
+                  <Link to="/"><span>7. 첨단선경</span></Link><span className="color-red">1▼</span>
                 </li>
                 <li>
-                  <span>8. 첨단과학산업기지라인3차</span><span className="color-red">신규</span>
+                  <Link to="/"><span>8. 첨단과학산업기지라인3차</span></Link><span className="color-red">신규</span>
                 </li>
                 <li>
-                  <span>9. 월계모아</span><span className="color-red">신규</span>
+                  <Link to="/"><span>9. 월계모아</span></Link><span className="color-red">신규</span>
                 </li>
                 <li>
-                  <span>10. 첨단산업기지라인2차</span><span className="color-red">신규</span>
+                  <Link to="/"><span>10. 첨단산업기지라인2차</span></Link><span className="color-red">신규</span>
                 </li>
               </ul>
             </div>
@@ -137,7 +137,7 @@ class Nowongu extends Component {
         <input type="radio" name="nowongu1" id="junggye1" />
         <input type="radio" name="nowongu1" id="woulgye1" />
         <div className="service-box">
-          <div className="service-list-box border-b">
+          <div className="service-list-box">
             <ul>
               <li className="margin-b-20 service-list-title">신고건수 top3 동</li>
               <li>
